54ba16bc7c2122446b3594aa9a40ecdfb632e4a6
ilham bouaik <ibouaik@gmail.com>
2022-06-30 19:58:08 +0100

    add routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,37 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import planet from '../images/planet.png'
+import './Navbar.css'
 const Navbar = () => {
   return (
-    <div className='navbar-container'>
-      <div className='logo'>
-        <img src={planet} />
-        <h2>Space Travelers' Hub</h2>
+    <nav>
+      <div className='navbar-left'>
+        <img className="image-icn" src={planet} />
+        <h2 className="nav-title">Space Travelers' Hub</h2>
       </div>
-    </div>
+      <div className="navbar-right">
+        <NavLink
+          to="/Rockets"
+          className={({ isActive }) => (isActive ? 'styled-link' : 'no-styled-link')}
+        >
+          Rockets
+        </NavLink>
+        <NavLink
+          to="/Missions"
+          className={({ isActive }) => (isActive ? 'styled-link' : 'no-styled-link')}
+        >
+          Missions
+        </NavLink>
+        <hr className="border " />
+        <NavLink
+          to="/Profile"
+          className={({ isActive }) => (isActive ? 'styled-link' : 'no-styled-link')}
+        >
+          Profile
+        </NavLink>
+      </div>
+    </nav>
+
   )
 }
 
